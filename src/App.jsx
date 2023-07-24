8dcff768b1be6c560278fc6f8bff59912f59a57b
tiago salgado <tiagosalgado1110@gmail.com>
2023-07-24 16:28:35 -0300

    V1.1 - item list container agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
-
+import Bienvenida from './components/itemListContainer';
 
 function App() {
 
   return (
 <main>
   <Navbar />
+  <Bienvenida proverbio="Hyakubun wa ikken ni shikazu"/>
+  <Bienvenida proverbio='"Ver algo una vez es mejor que escucharlo 100 veces"'/>
 </main>
   );
 }
---
+++ src/components/itemListContainer/index.jsx
@@ -0,0 +1,8 @@
+import "./styles.css"
+
+const Bienvenida = (props) => {
+    return <h2 className="proverbio">{props.proverbio}</h2>
+}
+
+
+export default Bienvenida
