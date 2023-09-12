1eb20f3a2c6126a2bf6dfd86fae367c2dc7bcc63
tiago salgado <tiagosalgado1110@gmail.com>
2023-09-12 13:31:40 -0300

    5.1 - chequeo de checkout

--- src/components/checkout/index.jsx
+++ src/components/checkout/index.jsx
@@ -9,30 +9,82 @@ import { CartContext } from '../../context/cartContext'
 import { useContext } from 'react'
 
 const Checkout = () => {
+    const [loading, setLoading] = useState(false)
     const [orderId, setOrderId] = useState('')
 
-    const {cart} = useContext(CartContext) 
-    const {total} = useContext(CartContext) 
+    const {cart, total, clearCart} = useContext(CartContext) 
 
-    const createOrder = async ({name, phone, email }) => {
-        
-        const objOrder = {
-            comprador : {
-                name, phone, email
-            },
-            items: cart,
-            total: total,
+
+    const createOrder = async ({name, phone, email}) => {
+        setLoading(true)
+
+        try{
+            const objOrder = {
+                comprador: {name, phone, email},
+                items: cart,
+                total: total,
+                date: Timestamp.fromDate(new Date())
+            }
+
+            const batch = writeBatch(db)
+
+            const outOfStock = []
+
+            const ids = cart.map(lentes => lentes.id)
+
+            const lentesRef = collection(db, 'VisualCheck-Products')
+
+            const lentesAddedFromFirestore = await getDocs(query(lentesRef, where(documentId(),'in',ids)))
+
+            const {docs} = lentesAddedFromFirestore
+
+            docs.forEach(doc => {
+                const dataDoc = doc.data()
+                const stockDb = dataDoc.stockDb
+
+                const lentesAddedToCart = cart.find(lentes => lentes.id === doc.id)
+                const lentesCantidad = lentesAddedToCart?.cantidad
+
+                if (stockDb >= lentesCantidad) {
+                    batch.update(doc.ref,{stock:stockDb- lentesCantidad})
+                } else {
+                    outOfStock.push({id:doc.id, ...dataDoc})
+                }
+            })
+
+            if (outOfStock.length !== 0) {
+                await batch.commit()
+
+                const orderRef = collection(db,'orders')
+
+                const orderAdded = await addDoc(orderRef, objOrder)
+
+                setOrderId(orderAdded.id)
+                clearCart()
+            } else{
+                console.error ('Disculpa, nos hemos quedado sin stock de algunos productos.')
+            }
+        } catch (error){
+            console.log(error)
+        } finally {
+            setLoading(false)
         }
-        setOrderId(objOrder)
-        console.log(orderId)
     }
-    
 
-  return (
-    <div>
-        <CheckoutForm onConfirm={createOrder}/>
-    </div>
-  )
+    if (loading) {
+        return 
+        <h1>Su orden esta siendo generada, espere un momento.</h1>
+    }
+    if (orderId) {
+        return <h1>El id de su orden es: {orderId}</h1>
+    }
+
+    return (
+        <div>
+            <h2>Checkout</h2>
+            <CheckOutForm onConfirm={createOrder}/>
+        </div>
+    )
 }
 
 export default Checkout
--- src/components/checkoutForm/index.jsx
+++ src/components/checkoutForm/index.jsx
@@ -6,92 +6,58 @@ import { TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 
 //Formik
-import { Formik } from "formik";
+
 
 //Yup (validation schema)
 import * as yup from "yup";
 
 // Components
-import Checkout from "../checkout/index";
+
 
 const yupSchema = yup
   .object({
-    Nombre: yup.string().min(2).max(30).required(),
-    Telefono: yup.number().min(10).required(),
-    Email: yup.string().email().required(),
+    name: yup.string().min(2).max(30).required(),
+    phone: yup.number().min(10).required(),
+    email: yup.string().email().required(),
   })
   .required();
 
 
 
-const CheckoutForm = (onConfirm) => {
-  const [data, setData] = useState({})
+  const CheckOutForm = ({onConfirm}) => {
+    const [name,setName] = useState('');
+    const [phone,setPhone] = useState('');
+    const [email,setEmail] = useState('');
 
-  //funcion que almacena datos y crea la orden
-  const submitHandler = (values, resetForm) => {
-    const userData = {
-      name: values.Nombre,
-      phone: values.Telefono,
-      email: values.Email,
-    };
-    
-    
-    resetForm();
-    setData(userData)
-    onConfirm(data)
-  };
-  console.log(typeof onConfirm)
-  console.log(data)
-  
-  return (
-    <div className="container-Form">
-      <h3>Completa el siguiente formulario para realizar una orden</h3>
-      <Formik
-        initialValues={{ Nombre: "", Telefono: "", Email: "" }}
-        onSubmit={(values, { resetForm }) => submitHandler(values, resetForm)}
-        validationSchema={yupSchema}
-      >
-        {({ values, handleChange, handleSubmit, isValid, dirty }) => (
-          <form onSubmit={handleSubmit} className="form">
-            <TextField
-              placeholder="Nombre y apellido"
-              variant="outlined"
-              name="Nombre"
-              className="TextField"
-              sx={{ mb: "10px" }}
-              value={values.Nombre}
-              onChange={handleChange}
-            />
-            <TextField
-              placeholder="Numero de telefono"
-              variant="outlined"
-              name="Telefono"
-              className="TextField"
-              sx={{ mb: "10px" }}
-              value={values.Telefono}
-              onChange={handleChange}
-            />
-            <TextField
-              placeholder="Email"
-              variant="outlined"
-              name="Email"
-              className="TextField"
-              value={values.Email}
-              onChange={handleChange}
-            />
-            <Button
-              variant="contained"
-              type="submit"
-              sx={{ marginTop: 5 }}
-              disabled={!(isValid && dirty)}
-            >
-              Enviar orden
-            </Button>
-          </form>
-        )}
-      </Formik>
-    </div>
-  );
-};
 
-export default CheckoutForm;
+    const handleConfirm = (event) =>{
+        event.preventDefault()
+
+        const userData = {name, phone, email}
+
+        onConfirm(userData);
+    }
+
+    return (
+        <div className='container-Form'>
+          <h3>Complete el siguiente formulario para reservar la orden de compra</h3>
+            <form onSubmit={handleConfirm} className='form'>
+                <label className='Label'>
+                    <TextField className='TextField' type='text' placeholder="Nombre y apellido" value={name} sx={{marginTop: 2}} onChange={({target}) => setName(target.value)}></TextField>
+                </label>
+                <label className='Label'>
+                    <TextField className='TextField' type='text' placeholder="Telefono" value={phone} sx={{marginTop: 2}} onChange={({target}) => setPhone(target.value)}></TextField>
+                </label>
+                <label className='Label'>
+                    <TextField className='TextField' type='text' placeholder="Email" value={email} sx={{marginTop: 2}} onChange={({target}) => setEmail(target.value)}></TextField>
+                </label>
+                <div className='Label'>
+                    <Button variant="contained" type='submit' className='TextField' sx={{marginTop: 2}}>Crear Orden</Button>
+                </div>
+            </form>
+        </div>
+
+    )
+}
+
+export default CheckOutForm;
